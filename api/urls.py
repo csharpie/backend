51e74d69a0360a86c05890aa24bb7da8e5f593cb
Brad Steinberg <steinbergbrad@gmail.com>
2018-05-19 12:31:21 -0400

    Renamed urls in urls.py and moved retrieve_token to api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,23 +2,25 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework_jwt.views import obtain_jwt_token
 
 schema_view = get_swagger_view(title='NearGreen API')
 
 urlpatterns = [
+    url(r'^analytics/locations_aggregation/$', views.AnalyticsLocationSummary.as_view()),
+    url(r'^analytics/locations_categories_aggregation/$',
+        views.AnalyticsLocationSummaryCategory.as_view()),
+    url(r'^authorization/retrieve_token/', obtain_jwt_token),
     url(r'^locations/$', views.LocationList.as_view()),
     url(r'^locations/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view()),
-    url(r'hours/$', views.HourList.as_view()),
-    url(r'hours/(?P<pk>[0-9]+)/$', views.HourDetail.as_view()),
-    url(r'^nearby_locations/?$', views.NearbyLocationList.as_view()),
-    url(r'locations/community_gardens/$',
+    url(r'^locations/community_gardens/$',
         views.CommunityGardenLocationList.as_view()),
-    url(r'locations/grocery_stores/$', views.GroceryStoreLocationList.as_view()),
-    url(r'locations/food_pantries/$', views.FoodPantryLocationList.as_view()),
-    url(r'locations/super_markets/$', views.SuperMarketLocationList.as_view()),
-    url(r'analytics/locations/$', views.AnalyticsLocationSummary.as_view()),
-    url(r'analytics/locations/categories/$',
-        views.AnalyticsLocationSummaryCategory.as_view()),
+    url(r'^locations/food_pantries/$', views.FoodPantryLocationList.as_view()),
+    url(r'^locations/grocery_stores/$', views.GroceryStoreLocationList.as_view()),
+    url(r'^locations/super_markets/$', views.SuperMarketLocationList.as_view()),
+    url(r'^hours/$', views.HourList.as_view()),
+    url(r'^hours/(?P<pk>[0-9]+)/$', views.HourDetail.as_view()),
+    url(r'^nearby_locations/?$', views.NearbyLocationList.as_view()),
     url(r'^$', schema_view)
 ]
 
--- backend/urls.py
+++ backend/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^', include('api.urls'))
 ]
